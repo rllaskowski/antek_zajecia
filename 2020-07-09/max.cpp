c1cbdbb138f2c4ad87eebb2ce68d1390649e03fd
rllaskowski <robert.laskowski0@gmail.com>
2020-07-09 15:33:46 +0200

    update

---
+++ 2020-07-09/max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int liczba1;
+    int liczba2;
+    cin >> liczba1 >> liczba2;
+
+    if (liczba1 > liczba2) {
+        cout << liczba1 << endl;
+    } else {
+        cout << liczba2 << endl;
+    }
+    
+
+    return 0;
+}
---
+++ 2020-07-09/wczytywanie.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    int wiekJanka;
+    int wiekAntka;
+
+    cin >> wiekJanka >> wiekAntka;
+
+    int sumaLat = wiekAntka+wiekJanka;
+
+    cout << "Janek i Antek mają razem tyle lat: ";
+
+    cout << sumaLat << endl; 
+
+    return 0;
+}
