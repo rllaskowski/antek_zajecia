85e9455ce94481c59ad4b8237f4fd9b7b60f65c5
rllaskowski <robert.laskowski0@gmail.com>
2020-07-11 16:50:12 +0200

    update

---
+++ 2020-07-11/gwiazdki.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int liczba;
+
+    cin >> liczba;
+
+    for (int i = 0; i < liczba; i++){
+        cout << "*";
+    }
+  
+    return 0;
+}
