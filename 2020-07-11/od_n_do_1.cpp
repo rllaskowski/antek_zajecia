12e330e6deecf91c02003bcda51b2dfee8a7ac8c
rllaskowski <robert.laskowski0@gmail.com>
2020-07-11 16:42:54 +0200

    update

---
+++ 2020-07-11/od_n_do_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int liczba;
+
+    cin >> liczba;
+
+    for (int i = liczba; i > 0; i--){
+        cout << i << endl;
+    }
+    return 0;
+}
